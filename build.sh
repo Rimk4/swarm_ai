16b8ecb7f7968a5df0726f0985b1bc19ae5698df
Rimk4 <romanovicpopa3@gmail.com>
2025-02-07 17:21:46 +0300

    refactor: переписал скрипт сборки исполняемого файла

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 DEST_DIR=build/
+SRC_DIR=src/
+AGENTS_DIR=agents/
+SWARM_DIR=swarm/
 TARGET_NAME=main
 
-g++ -o $TARGET_NAME main.cpp agents.cpp swarm.cpp robot.cpp
+g++ -o $TARGET_NAME ${SRC_DIR}main.cpp ${SRC_DIR}${AGENTS_DIR}agents.cpp \
+    ${SRC_DIR}${SWARM_DIR}swarm.cpp
 
-mkdir $DEST_DIR
+mkdir -p $DEST_DIR
 
-mv $TARGET_NAME $DEST_DIR
+mv -f $TARGET_NAME $DEST_DIR
